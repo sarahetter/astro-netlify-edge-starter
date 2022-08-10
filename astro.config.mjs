9856d262a2b3d8f5024a3d4afe74c8779874dec7
Prince Wilson <maxcell.wilson@gmail.com>
2022-08-09 20:05:59 -0400

    bump: Astro for v1 release (#2)
    * bump: astro for v1
    
    * fix: Adds output configuration for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: netlify()
+  adapter: netlify(),
+  output: 'server'
 });
