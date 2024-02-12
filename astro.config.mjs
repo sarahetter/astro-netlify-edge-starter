0c3d2aa744c3c4c97c568595c6ea0122442e7107
Mike Gualtieri <mike.gualtieri@gatsbyjs.com>
2024-02-12 15:13:31 -0500

    adjusting the syntax for the Astro edge functions adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/edge-functions';
+import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   output: 'server'
 });
